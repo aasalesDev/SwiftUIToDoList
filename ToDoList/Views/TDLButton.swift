91392b163d6d044c0150b6da634aa590fc8f0185
Anderson Sales <andersonsales.mg@gmail.com>
2023-05-08 00:13:56 -0300

    Create App Icon and Finish Register View

---
+++ ToDoList/Views/TDLButton.swift
@@ -0,0 +1,20 @@
+//
+//  TDLButton.swift
+//  ToDoList
+//
+//  Created by Anderson Sales on 07/05/23.
+//
+
+import SwiftUI
+
+struct TDLButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TDLButton_Previews: PreviewProvider {
+    static var previews: some View {
+        TDLButton()
+    }
+}
